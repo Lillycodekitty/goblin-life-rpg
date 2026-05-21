bd7db6c86ffd996fb86adab1bf2be3cdf77ea54b
Lillycodekitty <alishafouche07@gmail.com>
2026-05-21 11:34:59 +0200

    Update import path for app module

--- netlify/functions/api.mjs
+++ netlify/functions/api.mjs
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import app from "../../artifacts/api-server/dist/app.mjs";
+import app from "../../artifacts/api-server/dist/src/app.mjs";
 
 export const handler = serverless(app);
